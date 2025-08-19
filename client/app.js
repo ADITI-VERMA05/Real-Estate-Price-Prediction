5150904ad69f28df61baa082b2c7666a63f07136
ADITI_VERMA05 <aditiverma1605@gmail.com>
2025-08-19 23:33:03 +0530

    Final commit

---
+++ client/app.js
@@ -0,0 +1,57 @@
+function getBathValue() {
+    var uiBathrooms = document.getElementsByName("uiBathrooms");
+    for(var i in uiBathrooms) {
+        if(uiBathrooms[i].checked) {
+            return parseInt(uiBathrooms[i].value);
+        }
+    }
+    return -1; // Invalid Value
+}
+
+function getBHKValue() {
+    var uiBHK = document.getElementsByName("uiBHK");
+    for(var i in uiBHK) {
+        if(uiBHK[i].checked) {
+            return parseInt(uiBHK[i].value);
+        }
+    }
+    return -1; // Invalid Value
+}
+
+function onClickEstimatePrice() {
+    var sqft = document.getElementById("uiSqft").value;
+    var bhk = getBHKValue();
+    var bathrooms = getBathValue();
+    var location = document.getElementById("uiLocations").value;
+
+    var url = "http://127.0.0.1:5000/predict_home_price"; 
+
+    $.post(url, {
+        total_sqft: parseFloat(sqft),
+        bhk: bhk,
+        bath: bathrooms,
+        location: location
+    },function(data, status) {
+        console.log(data.estimated_price);
+        document.getElementById("uiEstimatedPrice").innerHTML = "<h2>" + data.estimated_price.toString() + " Lakh</h2>";
+    });
+}
+
+function onPageLoad() {
+    console.log("document loaded");
+    var url = "http://127.0.0.1:5000/get_location_names"; 
+    $.get(url,function(data, status) {
+        console.log("got response for get_location_names request");
+        if(data) {
+            var locations = data.locations;
+            var uiLocations = document.getElementById("uiLocations");
+            $('#uiLocations').empty();
+            for(var i in locations) {
+                var opt = new Option(locations[i]);
+                $('#uiLocations').append(opt);
+            }
+        }
+    });
+}
+
+window.onload = onPageLoad;
